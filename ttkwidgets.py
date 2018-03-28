9588506184ede785dd06342fc1527da284921fd0
Billy Do <dob@utexas.edu>
2018-03-28 16:48:49 -0500

    implemented a few widgets

--- ttkwidgets.py
+++ ttkwidgets.py
@@ -16,7 +16,6 @@ widgetNames = ("Button", "Checkbutton", "Entry", "Frame", "Label", "Labelframe",
 
 class App:
 	def __init__(self, master):
-		print("App.__init__(master)")
 		self.mainframe = ttk.Frame(master, padding=(5))
 		self.mainframe.pack()
 
@@ -26,25 +25,83 @@ class App:
 		self.widgetlist = Listbox(self.mainframe, listvariable=self.wnames, height=20)
 		self.widgetlist.grid(column=0, row=0)
 
-		self.widgetframe = ttk.Frame(self.mainframe, padding=(10))
+		self.widgetframe = ttk.Frame(self.mainframe, padding=(10), width="250px")
 		self.widgetframe.grid(column=1, row=0, columnspan=3)
 
-		self.testlabel = ttk.Label(self.widgetframe, text="This is a test label.")
-		self.testlabel.grid(column=0, row=0)
+		self.selected_widget = None
 
 		# Bind Listbox options to change the example widget
 		self.widgetlist.bind("<<ListboxSelect>>", self.switchExample)
 
 	def switchExample(self, event):
-		print("In App.switchExample")
-		old_widget = self.testlabel
+		old_widget = self.selected_widget
 
 		index = self.widgetlist.curselection()
-		newText = self.widgetlist.get(index) + " was selected."
-		self.testlabel = ttk.Label(self.widgetframe, text=newText)
-		old_widget.destroy()
-		self.testlabel.grid(column=0, row=0)
+		try:
+			widgname = self.widgetlist.get(index)
+			if widgname == "Button":
+				self.showButton()
+			elif widgname == "Checkbutton":
+				self.showCheckbutton()
+			elif widgname == "Entry":
+				self.showEntry()
+			elif widgname == "Frame":
+				self.showFrame()
+
+			if old_widget is not None:
+				old_widget.destroy()
+			self.selected_widget.grid(column=0, row=0)
+		except:
+			raise
 		return
+
+	def showButton(self):
+		options = { "text":"Button" }
+		self.selected_widget = ttk.Button(self.widgetframe, **options)
+
+	def showCheckbutton(self):
+		options = { "text":"Checkbutton" }
+		# v = IntVar()
+		self.selected_widget = ttk.Checkbutton(self.widgetframe, **options)
+		self.selected_widget.invoke() # toggles into "on" state
+		self.selected_widget.invoke() # toggles into "off" state
+
+	def showEntry(self):
+		entry_string = StringVar()
+		options = { "textvariable":entry_string }
+		self.selected_widget = ttk.Entry(self.widgetframe, **options)
+
+	def showFrame(self):
+		options = { "borderwidth":2,
+					"relief":"sunken",
+					"width":"250px",
+					"height":"150px"
+					}
+		self.selected_widget = ttk.Frame(self.widgetframe, **options)
+
+	def showLabel(self):
+		pass
+
+	def showLabelframe(self):
+		pass
+
+	def showMenubutton(self):
+		pass
+
+	def showPanedWindow(self):
+		pass
+
+	def showRadiobutton(self):
+		pass
+
+	def showScale(self):
+		pass
+
+	def showScrollbar(self):
+		pass
+
+	def showCombobox(self):
+		pass
 
 ######## End of App
 
